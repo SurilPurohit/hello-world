a7fe279c153622ff1677d46c10e1a8f786d1b3ae
Suril <suril.p@somaiya.edu>
2021-01-31 16:24:19 +0530

    props,Functional and Class Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="suril" heroName="Suru"/>
+      <Greet name="rishi" heroName="Rishi">
+        <p>This is child props</p>
+      </Greet>
+      <Welcome name="classComponent" heroName="Class Component"/>
     </div>
   );
 }
