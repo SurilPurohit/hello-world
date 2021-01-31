203e6214372a905725213124b40c608f20f07459
Suril <suril.p@somaiya.edu>
2021-01-31 16:41:24 +0530

    Props and State

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <p>This is child props</p>
       </Greet>
       <Welcome name="classComponent" heroName="Class Component"/>
+      <Message />
     </div>
   );
 }
